41610a011f8e6517ee32b865f1006a25940066da
Derek-Ashmore <dashmore@force66.com>
2016-12-15 05:59:01 -0600

    added more unit test coverage

--- btm-DropwizardHealthChecks-cassandra/src/test/java/guru/breakthemonolith/health/cassandra/CassandraHealthCheckTest.java
+++ btm-DropwizardHealthChecks-cassandra/src/test/java/guru/breakthemonolith/health/cassandra/CassandraHealthCheckTest.java
@@ -1,14 +1,26 @@
 package guru.breakthemonolith.health.cassandra;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.exception.ContextedRuntimeException;
 import org.apache.commons.lang3.reflect.FieldUtils;
-import org.junit.After;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Cluster.Initializer;
+import com.datastax.driver.core.Configuration;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CassandraHealthCheckTest {
@@ -20,15 +32,23 @@ public class CassandraHealthCheckTest {
 	@Mock
 	private Logger loggerMock;
 
+	@Mock
+	private TestCluster testCluster;
+
+	@Mock
+	private Initializer clusterInitializer;
+
 	private CassandraHealthCheck healthCheck;
 
 	@Before
 	public void setUp() throws Exception {
 		healthCheck = new CassandraHealthCheck(TEST_SERVER);
-	}
+		FieldUtils.writeField(healthCheck, "logger", loggerMock, true);
 
-	@After
-	public void tearDown() throws Exception {
+		List<InetSocketAddress> addressList = new ArrayList<InetSocketAddress>();
+		addressList.add(new InetSocketAddress("localhost", 16666));
+		Mockito.when(clusterInitializer.getContactPoints()).thenReturn(addressList);
+		testCluster = new TestCluster(clusterInitializer);
 	}
 
 	@Test
@@ -64,6 +84,52 @@ public class CassandraHealthCheckTest {
 		Assert.assertEquals(CassandraHealthCheck.DEFAULT_TEST_QUERY, FieldUtils.readField(healthCheck, "query", true));
 		Assert.assertEquals(TEST_SERVER, FieldUtils.readField(healthCheck, "serverName", true));
 		Assert.assertNull(FieldUtils.readField(healthCheck, "keySpace", true));
+	}
+
+	static class TestCluster extends Cluster {
+
+		RuntimeException exceptionToThrow;
+		int nbrTimesCloseCalled = 0;
+
+		public TestCluster(Initializer initializer) {
+			super(initializer);
+		}
+
+		public TestCluster(String name, List<InetSocketAddress> contactPoints, Configuration configuration) {
+			super(name, contactPoints, configuration);
+		}
+
+		@Override
+		public void close() {
+			nbrTimesCloseCalled++;
+			if (exceptionToThrow != null)
+				throw exceptionToThrow;
+		}
+		
+	}
+
+	@Test
+	public void testCloseQuietlyCluster() throws Exception {
+		MethodUtils.invokeMethod(healthCheck, true, "closeQuietly", testCluster);
+		Assert.assertEquals(1, testCluster.nbrTimesCloseCalled);
+	}
+
+	// @Test
+	// public void testCloseQuietlyClusterNull() throws Exception {
+	// MethodUtils.invokeMethod(healthCheck, true, "closeQuietly", (Cluster)
+	// null);
+	// Assert.assertEquals(0, testCluster.nbrTimesCloseCalled);
+	// }
+
+	@Test
+	public void testCloseQuietlyClusterException() throws Exception {
+		testCluster.exceptionToThrow = new RuntimeException("crap");
+		MethodUtils.invokeMethod(healthCheck, true, "closeQuietly", testCluster);
+		Assert.assertEquals(1, testCluster.nbrTimesCloseCalled);
+
+		ArgumentCaptor<ContextedRuntimeException> exCaptor = ArgumentCaptor.forClass(ContextedRuntimeException.class);
+		Mockito.verify(loggerMock).warn(Matchers.anyString(), exCaptor.capture());
+		Assert.assertEquals(3, exCaptor.getValue().getContextLabels().size());
 	}
 
 }
